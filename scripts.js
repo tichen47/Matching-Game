4074d3f378794a499001507152a9cabd02a46977
tichen47 <tichen47@gmail.com>
2019-06-23 16:44:58 -0500

    Add restart game feature

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
+/*jshint esversion: 6 */
+
 const cards = document.querySelectorAll('.memory-card'); 
+const newGame = document.querySelector('.restart');
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let remainCard = 16;
+let message = "Congratulation";
 
 function flipCard() {
     if(lockBoard) return;
@@ -16,12 +21,14 @@ function flipCard() {
         // First click
         hasFlippedCard = true;
         firstCard = this;
+        remainCard -= 1;
         return;
     } 
 
     // Second click
     hasFlippedCard = false;
     secondCard = this;
+    remainCard -= 1;
 
     checkForMatch();
 }
@@ -39,6 +46,12 @@ function flipBackFirst(){
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+    if (remainCard === 0) {
+        setTimeout(() => {
+            alert(message);
+            newBoard();
+        }, 500);
+    }
 }
 
 function unFlipCards() {
@@ -48,19 +61,26 @@ function unFlipCards() {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
         lockBoard = false;
-    }, 1000);
+        remainCard += 2;
+    }, 800);
 }
 
-function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
-}
 
-(function shuffle(){
+function shuffle(){
     cards.forEach(card => {
         let randomPos = Math.floor(Math.random() * 16);
         card.style.order = randomPos;
     });
-})();
+}
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+function newBoard() {
+    cards.forEach(card => card.classList.remove('flip'));
+    cards.forEach(card => card.addEventListener('click', flipCard));
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+    remainCard = 16;
+    shuffle(); 
+}
+
+newGame.addEventListener('click', newBoard);
+newBoard();
